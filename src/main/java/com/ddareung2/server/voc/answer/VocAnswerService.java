a1ada6571e3ae68198a341f509bc134af9673d69
ymkmoon <ymkmoon@naver.com>
2021-05-29 00:53:29 +0900

    fix: Update mismatch method name

--- src/main/java/com/ddareung2/server/voc/answer/VocAnswerService.java
+++ src/main/java/com/ddareung2/server/voc/answer/VocAnswerService.java
@@ -36,7 +36,7 @@ public class VocAnswerService {
 				
 	}
 	
-	public VocAnswerResponse findVocAnswerById(Long id) {
+	public VocAnswerResponse findVocAnswerByVocQuestionId(Long id) {
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		
